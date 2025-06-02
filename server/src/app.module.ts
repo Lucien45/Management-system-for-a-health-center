de8a308b5f45367753ec1e8699e1d4db6cc42097
Lucien <savakalucien@gmail.com>
2025-06-02 16:07:41 +0300

    HMS-9 setup environment for backend

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const nodeEnv = configService.get<string>('NODE_ENV');
+        console.log(nodeEnv);
+        const isDev = nodeEnv === 'developpement';
+        return {
+          type: 'mongodb',
+          url: configService.get<string>('DB_HOST'),
+          database: configService.get<string>('DB_NAME'),
+          useUnifiedTopology: true,
+          useNewUrlParser: true,
+          entities: ['dist/**/*.entity{.js,.ts}'],
+          autoLoadEntities: true,
+          synchronize: isDev,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
