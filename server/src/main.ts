7321881aaa43ceaf08b5d289386484850eb12068
Lucien <savakalucien@gmail.com>
2025-06-08 12:13:40 +0300

    HMS-13: use cookie sécurisé HTTP-only for securise l'auth user on frontend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/media', express.static(join(__dirname, '..', 'media')));
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,POST,PATCH,PUT,DELETE',
+    credentials: true,
+  });
 
   const port = parseInt(process.env.PORT || '3030', 10);
   await app.listen(port);
